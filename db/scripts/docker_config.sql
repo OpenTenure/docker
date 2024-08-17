c8b87442b188333d3608851fcec6b136c58b79c9
Alexander Solovov <a_solovov@yahoo.com>
2024-08-17 16:39:46 +0300

    Updating CS image and db image to reference postgresql utilities for backup and restore purposes

--- db/scripts/docker_config.sql
+++ db/scripts/docker_config.sql
@@ -1,3 +1,2 @@
-update system.setting set vl = '1' where name = 'enable-reports';
-update system.setting set vl = '' where name = 'db-utilities-folder';
+update system.setting set vl = '/usr/lib/psql' where name = 'db-utilities-folder';
 update system.config_map_layer set title='Claims', url='http://localhost:8085/geoserver/opentenure/wms', wms_layers='opentenure:claims' where name = 'claims-orthophoto';
